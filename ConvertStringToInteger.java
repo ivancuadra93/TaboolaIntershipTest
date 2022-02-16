88351c25074a05f0549d286df2ee86ab4a89a69f
Ivan Cuadra <ivan.cuadra.93@gmail.com>
2022-02-16 00:16:59 -0800

    Add DetectIntegerInString Class content

--- ConvertStringToInteger.java
+++ ConvertStringToInteger.java
@@ -4,7 +4,6 @@ import java.lang.Math;
 // Write a Java program that take a string input and convert it to an integer without
 // using the build-in parse function.
 // Example: input value “123”, convert it to an integer type with value 123
-
 public class ConvertStringToInteger {
     
     public static int convert(String userString) {
--- DetectIntegerInString.java
+++ DetectIntegerInString.java
@@ -1,7 +1,49 @@
+import java.util.HashMap;
+
+// Write a Java program that take a input and detect whether there’s integer in
+// there.
+// Example: input value “45222” return true, input value “This Is A Test4me” return
+// true, input value “IAMGOOD” return false
 public class DetectIntegerInString {
 
     public static boolean detect(String userString) {
-        return true;
+        int stringLen = userString.length();
+        boolean result = false;
+
+        
+        if(userString == null || stringLen == 0){
+            System.out.println("User string is null or empty. Returning false...\n");
+        }
+        else{
+            char[] charArray = userString.toCharArray();
+            HashMap<Character, Integer> stringToInteger = new HashMap<Character, Integer>(){{
+                put('0', 0);
+                put('1', 1);
+                put('2', 2);
+                put('3', 3);
+                put('4', 4);
+                put('5', 5);
+                put('6', 6);
+                put('7', 7);
+                put('8', 8);
+                put('9', 9);
+            }};
+
+            for(int i = 0; i < stringLen; i++){
+                if(stringToInteger.containsKey(charArray[i])){
+                    result = true;
+                    break;
+                }
+            }
+            if(result){
+                System.out.println("The user inputted string contains at least one number. Returning true...\n");
+            }
+            else {
+                System.out.println("The user inputted string does NOT contain a number. Returning true...\n");
+            }
+
+        }
+        
+        return result;
     }
-    
 }
